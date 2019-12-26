96f5381c32309786338d359ef16b94ae4802936e
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2019-12-26 13:17:39 +0900

    新規ボランティア登録画面

--- b_vol_regd/b_vol_regd_new.php
+++ b_vol_regd/b_vol_regd_new.php
@@ -1,39 +1,12 @@
-<?php
-
-header("Content-type: text/html; charset=utf-8");
-
-require_once("db.php");
-$mysqli = db_connect();
-
-$sql = "SELECT * FROM vol";
-
-$result = $mysqli -> query($sql);
-
-//クエリ失敗
-if(!$result) {
-	echo $mysqli->error;
-	exit();
-}
-
-//連想配列で取得
-while($row = $result->fetch_array(MYSQLI_ASSOC)){
-	$rows[] = $row;
-}
-
-//結果セットを解放
-$result->free();
-
-// データベース切断
-$mysqli->close();
-
-?>
-<!DOCTYPE html> <!-- 宣言（無くても機能する？） -->
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8"> <!-- 文字コードを宣言 -->
   <title>Sample</title> <!-- ページのタイトル -->
   <link rel="stylesheet" type="text/css" href="./CSS/common.css">
   <link rel="stylesheet" type="text/css" href="./CSS/vol_regd.css">
+	<script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+
 </head>
 <body>
   <div id="header-fixed">
@@ -46,34 +19,90 @@ $mysqli->close();
       <div id="Toptitle1">
         登録・編集
       </div>
-      <table border='1'>
+			<div width="100%">
+				<form action="receiver.php" method="post">
+		  		<label>ボランティア名</label><br>
+		  		<input type="text" name="vol_name" maxlength="20" value="" placeholder="２０文字以内で入力">
+					<br>
+					<h2>地域選択</h2>
+					<label>都道府県</label>
+					<select name="pull-down">
+				    <option value="elephant">ゾウさん</option>
+				    <option value="lion">ライオンさん</option>
+				    <option value="penguin">ペンギンさん</option>
+				  </select>
+					<br>
+					<label>地域</label>
+					<select name="pull-down">
+				    <option value="elephant">ゾウさん</option>
+				    <option value="lion">ライオンさん</option>
+				    <option value="penguin">ペンギンさん</option>
+				  </select>
+					<br>
+					<h2>郵便番号</h2>
+					<!-- ▼郵便番号入力フィールド(7桁) -->
+					<input type="text" name="zip11" size="10" maxlength="8" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11');">
+					<!-- ▼住所入力フィールド(都道府県+以降の住所) -->
+					<br>
+					<h2>住所</h2>
+					<input type="text" name="addr11" size="60">
+					<br>
+					<h2>開催日</h2>
+					<select name="year">
+				    <option value="2019">2019</option>
+				    <option value="2020">2020</option>
+				  </select>
+					年
+					<select name="month">
+				    <option value="1">1</option>
+				    <option value="2">2</option>
+						<option value="3">3</option>
+				    <option value="4">4</option>
+						<option value="5">5</option>
+				    <option value="6">6</option>
+						<option value="7">7</option>
+				    <option value="8">8</option>
+						<option value="9">9</option>
+				    <option value="10">10</option>
+						<option value="11">11</option>
+				    <option value="12">12</option>
+					</select>
+						月
+						<select name="day">
+					    <option value="1">1</option>
+					    <option value="2">2</option>
+							<option value="3">3</option>
+					    <option value="4">4</option>
+							<option value="5">5</option>
+					    <option value="6">6</option>
+							<option value="7">7</option>
+					    <option value="8">8</option>
+							<option value="9">9</option>
+					    <option value="10">10</option>
+							<option value="11">11</option>
+					    <option value="12">12</option>
+				  </select>
+					日
+					<br>
+					<label>開始時間</label>
+					<input type="time" name="beg_time">
+					<br>
+					<label>終了時間</label>
+					<input type="time" name="fin_time">
 
-<?php
-foreach($rows as $row){
- ?>
+					<h2>定員</h2>
+					<input type="number" name="age" value="">名
+					<br>
+					<input id="check-a" type="checkbox" name="f" value="スポーツ" checked><label for="check-a">報酬あり</label><br>
+					(※詳しい報酬内容は詳細にお書きください)<br>
+					<input id="check-a" type="checkbox" name="f" value="スポーツ" checked><label for="check-a">初心者歓迎</label><br>
+					<label>詳細</label><br>
+  				<textarea name="message-body" placeholder="詳細を入力"></textarea>
+					<br>
+					<input type="submit" value="登録">
+				</form>
 
-<tr>
-	<td class="volList"><?=$row['id']?></td>
-	<td class="voLlist"><?=htmlspecialchars($row['vol_name'], ENT_QUOTES, 'UTF-8')?></td>
-	<td>
-		<form action="update2.php" method="post">
-		<input type="submit" value="変更する">
-		<input type="hidden" name="id" value="<?=$row['id']?>">
-		</form>
-	</td>
-  <td>
-		<form action="delete2.php" method="post">
-		<input type="submit" value="削除する">
-		<input type="hidden" name="id" value="<?=$row['id']?>">
-		</form>
-	</td>
-</tr>
-
- <?php
- }
- ?>
-
-</table>
+			</div>
     </div>
   </div>
   <div id="footer-fixed">
