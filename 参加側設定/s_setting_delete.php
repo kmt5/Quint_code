9fa5f75d497ee0219b1b49bb16c273281f9f8a84
210360n <43773607+210360n@users.noreply.github.com>
2019-12-30 16:59:15 +0900

    Create s_setting_delete.php

---
+++ 参加側設定/s_setting_delete.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> <!-- 宣言（無くても機能する？） -->
+<html>
+<head>
+  <meta charset="utf-8"> <!-- 文字コードを宣言 -->
+  <title>アカウント削除</title> <!-- ページのタイトル -->
+  <link rel="stylesheet" type="text/css" href="./CSS/common.css">
+  <link rel="stylesheet" type="text/css" href="./CSS/color.css">
+  <link rel=“stylesheet” type="text/css" href=“./CSS/size.css”>
+</head>
+<body>
+  <div id="header-fixed">
+    <img border="0" src="header.jpg" width="100%" height="100%">
+  </div>
+  <div id="body-bk">
+    <div id="body">
+      <center> <!-- 中央寄せ -->
+        <h2>
+      アカウントの削除を行います<br>
+      削除したアカウントはもう一度<br>
+      ログインすることができず<br>
+      データは削除されます<br>
+      <a href="login.html" class="btn-square">削除する</a>
+      <a href="s_setting_edit.php" class="btn-square">削除しない</a><br>
+    </h2>
+    </center>
+    </div>
+  </div>
+  <div id="footer-fixed">
+    <img border="0" src="kokoku.jpg" width="100%" height="100%">
+  </div>
+</body>
+</html>
