9caf1d081a41c4d6506e2d9d4011b06c21eeb5a8
210360n <43773607+210360n@users.noreply.github.com>
2020-01-12 20:59:30 +0900

    Add files via upload

--- 参加側設定/s_setting_delete.php
+++ 参加側設定/s_setting_delete.php
@@ -9,22 +9,35 @@
 </head>
 <body>
   <div id="header-fixed">
-    <img border="0" src="header.jpg" width="100%" height="100%">
+    <img border="0" src="header.jpg"style="vertical-align:middle;" width="100%" height="100%">
+    <a href= "s_setting_edit.php">
+      <img border="0" src="back.jpg" width="20%" height="100%" class="back">
+    </a>
+    <a href= "s_home.html">
+      <img border="0" src="home.jpg" width="20%" height="100%" class="home">
+    </a>
   </div>
+
   <div id="body-bk">
-    <div id="body">
+    <div id="body" class="bg_test2">
       <center> <!-- 中央寄せ -->
-        <h2>
+        <h1> 参加側アカウント設定</h1>
+      </center>
+      </div>
+    <div id="body" class="size3">
+      <center> <!-- 中央寄せ -->
       アカウントの削除を行います<br>
       削除したアカウントはもう一度<br>
       ログインすることができず<br>
       データは削除されます<br>
-      <a href="login.html" class="btn-square">削除する</a>
-      <a href="s_setting_edit.php" class="btn-square">削除しない</a><br>
-    </h2>
+</div>
+<div id="body" class="size2">
+  <center> <!-- 中央寄せ -->
+      <a href="login.php" class="btn-square4">削除する</a>
+      <a href="s_setting_edit.php" class="btn-square5">削除しない</a><br>
     </center>
     </div>
-  </div>
+    </div>
   <div id="footer-fixed">
     <img border="0" src="kokoku.jpg" width="100%" height="100%">
   </div>
