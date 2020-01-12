6fd649daf3cf9d16468db188a14d6ecc41f86459
210360n <43773607+210360n@users.noreply.github.com>
2020-01-12 21:08:07 +0900

    Add files via upload

---
+++ 募集側設定/b_setting_delete.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html> <!-- 宣言（無くても機能する？） -->
+<html>
+<head>
+  <meta charset="utf-8"> <!-- 文字コードを宣言 -->
+  <title>アカウント削除</title> <!-- ページのタイトル -->
+  <link rel="stylesheet" type="text/css" href="./CSS/common.css">
+  <link rel="stylesheet" type="text/css" href="./CSS/color.css">
+  <link rel=“stylesheet” type="text/css" href=“./CSS/size.css”>
+</head>
+<body>
+  <div id="header-fixed">
+    <img border="0" src="header.jpg"style="vertical-align:middle;" width="100%" height="100%">
+    <a href= "b_setting_edit.php">
+      <img border="0" src="back.jpg" width="20%" height="100%" class="back">
+    </a>
+    <a href= "b_home.html">
+      <img border="0" src="home.jpg" width="20%" height="100%" class="home">
+    </a>
+  </div>
+
+  <div id="body-bk">
+    <div id="body" class="bg_test2">
+      <center> <!-- 中央寄せ -->
+        <h1> 募集側アカウント設定</h1>
+      </center>
+      </div>
+    <div id="body" class="size3">
+      <center> <!-- 中央寄せ -->
+      アカウントの削除を行います<br>
+      削除したアカウントはもう一度<br>
+      ログインすることができず<br>
+      データは削除されます<br>
+</div>
+<div id="body" class="size2">
+  <center> <!-- 中央寄せ -->
+      <a href="login.php" class="btn-square4">削除する</a>
+      <a href="b_setting_edit.php" class="btn-square5">削除しない</a><br>
+    </center>
+    </div>
+    </div>
+  <div id="footer-fixed">
+    <img border="0" src="kokoku.jpg" width="100%" height="100%">
+  </div>
+</body>
+</html>
