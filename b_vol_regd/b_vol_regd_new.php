073190b89629cff8a10fe3c5e261f17f16d03459
kmt5 <43773627+kmt5@users.noreply.github.com>
2020-01-13 23:38:45 +0900

    Summary

--- b_vol_regd/b_vol_regd_new.php
+++ b_vol_regd/b_vol_regd_new.php
@@ -1,4 +1,36 @@
+<!--ボランティア新規登録画面-->
 <!DOCTYPE html>
+<?php
+    //データベースに接続(test3)
+    $dsn = "mysql:host=test3_mysql_1;dbname=sample;";
+    $db = new PDO($dsn, 'root', 'root');
+    //接続確認    
+    if ($db) {
+        echo "データベースに繋がっています";
+    } else {
+        "データベースに繋がってないです";
+    }
+
+    //都道府県プルダウン
+    if ($pref_data = $db -> query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
+      foreach($pref_data as $pref_data_val) {
+        $pref_pd .= "<option value='".$pref_data_val['pref_id']."'>".$pref_data_val['pref_name']."</option>";
+      }
+    }
+?>
+<?php
+if(isset($_FILES)&& isset($_FILES['image']) && is_uploaded_file($_FILES['image']['tmp_name'])){
+    if(!file_exists('upload')){
+        mkdir('upload');
+    }
+    $a = 'upload/' . basename($_FILES['image']['name']);
+    if(move_uploaded_file($_FILES['image']['tmp_name'], $a)){
+        $msg = $a. 'のアップロードに成功しました';
+    }else {
+        $msg = 'アップロードに失敗しました';
+    }
+}?>
+
 <html>
 <head>
   <meta charset="utf-8"> <!-- 文字コードを宣言 -->
@@ -22,23 +54,34 @@
       </div>
 			<div width="100%" class="new">
         <h1 align="center">ボランティア内容</h1>
-				<form action="receiver.php" method="post" align="left">
+        <form method="POST" action="b_vol_regd_new.php" enctype="multipart/form-data">
+        <input type="file" name="image" accept="image/*">
+
 		  		<h2>ボランティア名</h2>
 		  		<input type="text" name="vol_name" maxlength="20" value="" placeholder="２０文字以内で入力">
 					<br>
 					<h2>地域選択</h2>
-					<label>都道府県　</label>
-					<select name="pull-down">
-				    <option value="">ゾウさん</option>
-				    <option value="lion">ライオンさん</option>
-				    <option value="penguin">ペンギンさん</option>
-				  </select>
-					<br><br>
-					<label>地域　　　</label>
-					<select name="pull-down">
-				    <option value="elephant">ゾウさん</option>
-				    <option value="lion">ライオンさん</option>
-				    <option value="penguin">ペンギンさん</option>
+					<label>都道府県</label>
+					<select name="select_pref", id="pref">
+          <option value="none" selected>----選択してください----</option>
+          <?php
+          echo $pref_pd;
+          ?>
+          </select>
+          <br><br>
+          <script>
+            $('#pref').change(function() {
+              $.get('arealist.php?pref_id='+$("#pref").val(), function(data) {
+                $('#area').html(data);
+              });
+              $('#area').val('');
+              $('#area').selectmenu('refresh'); 
+            });
+          </script>
+					<label>地域</label>
+					<select name="select_area", id="area">
+          <option value="none" selected></option>
+          
 				  </select>
 					<br>
 					<h2>郵便番号</h2>
@@ -52,10 +95,9 @@
 					<br>
 					<h2>開催日</h2>
           <select id="year" name="year">
-            <option value="2019">2019</option>
             <option value="2020">2020</option>
           </select> 年
-          <select id"month" name="month">
+          <select id="month" name="month">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -109,7 +151,7 @@
 					<label>終了時間</label>
 					<input type="time" name="fin_time">
 					<h2>定員</h2>
-					<input class="capacity" type="number" name="age" value="" maxlength="3">名
+					<input class="capacity" type="number" name="capacity" value="" maxlength="3">名
 					<br><br>
 					<input id="check-a" type="checkbox" name="va_flag" value="1" checked><label for="check-a">報酬あり</label><br>
 					(※詳しい報酬内容は<br>　　詳細にお書きください)<br><br>
@@ -121,14 +163,12 @@
 				    <option value="penguin">シルバー</option>
             <option value="lion">ゴールド</option>
 				  </select><br><br>
-          <h2>ボランティアイメージ画像</h2>
-          <input type="file" name="pic" accept=".jpg,.png,image/jpeg,image/png">
-          <br><br>
+
 					<h2>詳細</h2>
   				<textarea name="detail" placeholder="詳細を入力"></textarea>
 					<br>
-					<button type="submit" align="center">登録</button>
-				</form>
+          <button type="submit" align="center">登録</button>
+        </form>
 			</div>
     </div>
   </div>
@@ -137,3 +177,78 @@
   </div>
 </body>
 </html>
+
+<?php
+if(isset($msg) && $msg == true){
+    echo '<p>'. $msg . '</p>';
+}
+?>
+<?php
+$h = apache_request_headers();
+var_dump($h["Content-Type"]);
+?>
+<?php
+
+// 入力フォームのデータを変数に
+$vol_is = null;
+$vol_name = $_POST["vol_name"];
+$vol_date .= $_POST["year"]."-".$_POST["month"]."-".$_POST["day"];
+$vol_beg_time = $_POST["beg_time"];
+$vol_fin_time = $_POST["fin_time"];
+$vol_capacity = $_POST["capacity"];
+$post_num = $_POST["zip11"];
+$vol_place = $_POST["addr11"];
+$val_flag = $_POST["va_flag"];
+$newbie_flag = $_POST["newbie_flag"];
+$vol_detail = $_POST["detail"];
+$pref_id = $_POST["select_pref"];
+$spec_rank = $_POST["rank_spec"];
+$b_user_id = '00000001';
+$rank_spec_flag = 0;
+$area_id = $_POST["select_area"];
+$point = 1;
+$vol_fig_pass = $a;
+$disapp_flag = 0;
+
+//データベースに接続(test3)
+$dsn = "mysql:host=test3_mysql_1;dbname=sample;";
+$db = new PDO($dsn, 'root', 'root');
+//接続確認    
+if ($db) {
+    echo "データベースに繋がっています";
+} else {
+    "データベースに繋がってないです";
+}
+//データを登録する準備
+
+$regist = $db->prepare("INSERT INTO volunteers(vol_id, vol_name, vol_date, vol_beg_time, vol_fin_time, vol_capacity, post_num, vol_place, val_flag, newbie_flag, vol_detail, pref_id, spec_rank, b_user_id, rank_spec_flag, area_id, vol_point, vol_fig_pass, disapp_flag) VALUES (:vol_id, :vol_name, :vol_date, :vol_beg_time, :vol_fin_time, :vol_capacity, :post_num, :vol_place, :val_flag, :newbie_flag, :vol_detail, :pref_id, :spec_rank, :b_user_id, :rank_spec_flag, :area_id, :vol_point, :vol_fig_pass, :disapp_flag)");
+//値を格納します
+$regist->bindValue(":vol_id", null, PDO::PARAM_INT);
+$regist->bindParam(":vol_name", $vol_name, PDO::PARAM_STR);
+$regist->bindValue(":vol_date", date('Y-m-d', strtotime($vol_date)),PDO::PARAM_STR);
+$regist->bindValue(":vol_beg_time", date("H:i", strtotime($vol_beg_time)), PDO::PARAM_STR);
+$regist->bindValue(":vol_fin_time", date("H:i", strtotime($vol_fin_time)), PDO::PARAM_STR);
+$regist->bindValue(":vol_capacity", $vol_capacity, PDO::PARAM_INT);
+$regist->bindParam(":post_num", $post_num, PDO::PARAM_STR);
+$regist->bindParam(":vol_place", $vol_place, PDO::PARAM_STR);
+$regist->bindValue(":val_flag", $val_flag, PDO::PARAM_INT);
+$regist->bindValue(":newbie_flag", $newbie_flag, PDO::PARAM_INT);
+$regist->bindParam(":vol_detail", $vol_detail, PDO::PARAM_STR);
+$regist->bindParam(":pref_id", $pref_id, PDO::PARAM_STR);
+$regist->bindValue(":spec_rank", $spec_rank, PDO::PARAM_INT);
+$regist->bindParam(":b_user_id", $b_user_id, PDO::PARAM_STR);
+$regist->bindParam(":rank_spec_flag", $rank_spec_flag);
+$regist->bindValue(":area_id", $area_id, PDO::PARAM_INT);
+$regist->bindValue(":vol_point", $point, PDO::PARAM_INT);
+$regist->bindParam(":vol_fig_pass", $vol_fig_pass, PDO::PARAM_STR);
+$regist->bindValue(":disapp_flag", $disapp_flag, PDO::PARAM_INT);
+
+//実行
+$regist->execute();
+if ($regist) {
+echo "登録完了!";
+} else {
+echo "エラーです！";
+}
+$db=null;
+?>
