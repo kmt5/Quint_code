d7764a08f9e9486ce4327f7c4565ba5f876469b5
210360n <43773607+210360n@users.noreply.github.com>
2020-01-14 04:59:16 +0900

    Add files via upload

---
+++ QR募集/b_qr_choice.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html> <!-- 宣言（無くても機能する？） -->
+<html>
+<head>
+  <meta charset="utf-8"> <!-- 文字コードを宣言 -->
+  <title>PHP</title> <!-- ページのタイトル -->
+  <link rel="stylesheet" type="text/css" href="./CSS/common.css">
+  <link rel="stylesheet" type="text/css" href="./CSS/color.css">
+  <link rel=“stylesheet” type="text/css" href=“./CSS/size.css”>
+  <link rel=“stylesheet” type="text/css" href=“./CSS/pop.css”>
+</head>
+<body>
+  <div id="header-fixed">
+    <img border="0" src="header.jpg"style="vertical-align:middle;" width="100%" height="100%">
+    <a href= "b_home.html">
+      <img border="0" src="back.jpg" width="20%" height="100%" class="back">
+    </a>
+    <a href= "b_home.html">
+      <img border="0" src="home.jpg" width="20%" height="100%" class="home">
+    </a>
+  </div>
+
+  <div id="body-bk">
+    <div id="body" class="bg_test3">
+      <center> <!-- 中央寄せ -->
+        <h1>QRコード</h1>
+      </center>
+    </div>
+    <div id="body">
+      <center> <!-- 中央寄せ -->
+        <h1>ボランティア一覧</h1>
+      </center>
+    </div>
+    <div id="body-qr1" class="bg_test3 size5">
+        <center> <!-- 中央寄せ -->
+          <h1>こんにちは</h1><br>
+        </center>
+    </div>
+
+
+  </div>
+
+  <div id="footer-fixed">
+    <img border="0" src="kokoku.jpg" width="100%" height="100%">
+  </div>
+</body>
+</html>
