24086235bc794393b21c3946a3d809d239142382
kmt5 <43773627+kmt5@users.noreply.github.com>
2020-01-16 07:03:58 +0900

    apd

--- 検索/s_search_area_spe.php
+++ 検索/s_search_area_spe.php
@@ -1,75 +1,139 @@
-<!DOCTYPE html> <!-- 宣言（無くても機能する？） -->
+<!--ボランティア新規登録画面-->
+<!DOCTYPE html>
+<?php
+//データベースに接続(test3)
+$dsn = "mysql:host=test3_mysql_1;dbname=sample;";
+$db = new PDO($dsn, 'root', 'root');
+//接続確認    
+if ($db) {
+  echo "データベースに繋がっています";
+} else {
+  "データベースに繋がってないです";
+}
+
+//都道府県プルダウン
+if ($pref_data = $db->query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
+  foreach ($pref_data as $pref_data_val) {
+    $pref_pd .= "<option value='" . $pref_data_val['pref_id'] . "'>" . $pref_data_val['pref_name'] . "</option>";
+  }
+}
+?>
+
 <html>
+
 <head>
   <meta charset="utf-8"> <!-- 文字コードを宣言 -->
-  <title>検索</title> <!-- ページのタイトル -->
+  <title>Sample</title> <!-- ページのタイトル -->
   <link rel="stylesheet" type="text/css" href="./CSS/common.css">
   <link rel="stylesheet" type="text/css" href="./CSS/search_first.css">
   <link rel="stylesheet" type="text/css" href="./CSS/search_area.css">
-  <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-
+  <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
+
 <body>
   <div id="header-fixed">
-    <img border="0" src="../common/header.jpg" width="100%" height="100%">
-    <a href="javascript:history.back()">
-      <p id="back"><i class="fas fa-reply"></i></p>
-    </a>
-    <a href="s_home">
-      <p id="home"><i class="fas fa-home"></i></p>
-    </a>
+    <img border="0" src="header.jpg" width="100%" height="100%">
+    <img border="0" src="back.jpg" width="20%" height="100%" class="back">
+    <img border="0" src="home.jpg" width="20%" height="100%" class="home">
   </div>
-
-  <div id = "body-bk">
-    <div id = "body">
-      <div id ="Toptitle1">
-        検索
+  <div id="body-bk">
+    <div id="body">
+      <div id="Toptitle1">
+        <i class="fas fa-edit"></i>　検索
       </div>
-
-      <center>
-          <select name="pre" id="pre" class="custom-select sources" placeholder="都道府県を選択してください">
-            <option value="01">北海道</option>
-            <option value="02">青森県</option>
+      <div width="100%" class="new">
+        <form method="POST" action="s_search_result.php" enctype="multipart/form-data">
+          <select name="select_pref" , id="pref">
+            <option value="none" selected>--都道府県を選択してください--</option>
+            <?php
+            echo $pref_pd;
+            ?>
           </select>
-
-          <select name="city" id="city" class="custom-select sources" placeholder="地域を選択してください">
+          <br><br>
+          <script>
+            $('#pref').change(function() {
+              $.get('arealist.php?pref_id=' + $("#pref").val(), function(data) {
+                $('#area').html(data);
+              });
+              $('#area').val('');
+              $('#area').selectmenu('refresh');
+            });
+          </script>
+          <select name="select_area" , id="area">
+            <option value="none" selected>--地域を選択してください--</option>
           </select>
 
           <div class="days">
-            <select id="year" name="year" class="custom1-select sources" placeholder="2019年　">
-              <option value="2019">2019年</option>
-              <option value="2020">2020年</option>
-            </select>
-
-            <select id"month" name="month" class="custom1-select sources" placeholder="1月">
-              <option value="1">1月</option>
-              <option value="2">2月</option>
-              <option value="3">3月</option>
-              <option value="4">4月</option>
-            </select>
-
-            <select id="day" name="day" class="custom1-select sources" placeholder="1日">
-              <option value="1">1日</option>
-              <option value="2">2日</option>
-              <option value="3">3日</option>
-              <option value="4">4日</option>
-            </select>
+            <select id="year" name="year" placeholder="2020　">
+              <option value="2020">2020</option>
+            </select>年
+            <select id="month" name="month" placeholder="1">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </select>月
+            <select id="day" name="day" placeholder="1">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="4">5</option>
+              <option value="4">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+              <option value="18">18</option>
+              <option value="19">19</option>
+              <option value="20">20</option>
+              <option value="21">21</option>
+              <option value="22">22</option>
+              <option value="23">23</option>
+              <option value="24">24</option>
+              <option value="25">25</option>
+              <option value="26">26</option>
+              <option value="27">27</option>
+              <option value="28">28</option>
+              <option value="29">29</option>
+              <option value="30">30</option>
+              <option value="31">31</option>
+            </select>日
           </div>
-
           <div class="checkbox">
-            <input type="checkbox" name="q1" value="value_flag"> 報酬アリ
-            <input type="checkbox" name="q1" value="vol_date"> 開催日が近い
+            <input type="checkbox" name="q1" value="val_flag"> 報酬アリ
+            <input type="checkbox" name="q1" value="vol_date_near"> 開催日が近い
             <input type="checkbox" name="q1" value="newbie_flag"> 初心者OK
           </div>
-
-          <a href="s_search_result.html" class="btn-square">検索</a>
-      </center>
+          <button class="search" type="submit" align="center">登録</button>
+        </form>
+      </div>
     </div>
   </div>
+  <div id="footer-fixed">
+    <img border="0" src="kokoku.jpg" width="100%" height="100%">
+  </div>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script type="text/javascript">
-/*都道府県preの値から地域の選択項目決定 参考サイト参照推奨
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script type="text/javascript">
+  /*都道府県preの値から地域の選択項目決定 参考サイト参照推奨
   $('#pre').change(function() {
     $.get('citylist.php?pref_code='+$(this).val(), function(data) {
       $('#city').html(data);
@@ -79,84 +143,81 @@
   $('#city').selectmenu('refresh');
 });*/
 
-/*プルダウン作成用スクリプト*/
-$(".custom-select").each(function() {
-  var classes = $(this).attr("class"),
-      id      = $(this).attr("id"),
-      name    = $(this).attr("name");
-  var template =  '<div class="' + classes + '">';
-      template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
-      template += '<div class="custom-options">';
-      $(this).find("option").each(function() {
-        template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
-      });
-  template += '</div></div>';
+  /*プルダウン作成用スクリプト*/
+  $(".custom-select").each(function() {
+    var classes = $(this).attr("class"),
+      id = $(this).attr("id"),
+      name = $(this).attr("name");
+    var template = '<div class="' + classes + '">';
+    template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
+    template += '<div class="custom-options">';
+    $(this).find("option").each(function() {
+      template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+    });
+    template += '</div></div>';
 
-  $(this).wrap('<div class="custom-select-wrapper"></div>');
-  $(this).hide();
-  $(this).after(template);
-});
-$(".custom-option:first-of-type").hover(function() {
-  $(this).parents(".custom-options").addClass("option-hover");
-}, function() {
-  $(this).parents(".custom-options").removeClass("option-hover");
-});
-$(".custom-select-trigger").on("click", function() {
-  $('html').one('click',function() {
-    $(".custom-select").removeClass("opened");
+    $(this).wrap('<div class="custom-select-wrapper"></div>');
+    $(this).hide();
+    $(this).after(template);
   });
-  $(this).parents(".custom-select").toggleClass("opened");
-  event.stopPropagation();
-});
-$(".custom-option").on("click", function() {
-  $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
-  $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
-  $(this).addClass("selection");
-  $(this).parents(".custom-select").removeClass("opened");
-  $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
-});
-
-/*日付プルダウン作成用スクリプト*/
-$(".custom1-select").each(function() {
-  var classes = $(this).attr("class"),
-      id      = $(this).attr("id"),
-      name    = $(this).attr("name");
-  var template =  '<div class="' + classes + '">';
-      template += '<span class="custom1-select-trigger">' + $(this).attr("placeholder") + '</span>';
-      template += '<div class="custom1-options">';
-      $(this).find("option").each(function() {
-        template += '<span class="custom1-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
-      });
-  template += '</div></div>';
-
-  $(this).wrap('<div class="custom1-select-wrapper"></div>');
-  $(this).hide();
-  $(this).after(template);
-});
-
-$(".custom1-option:first-of-type").hover(function() {
-  $(this).parents(".custom1-options").addClass("option-hover");
-}, function() {
-  $(this).parents(".custom1-options").removeClass("option-hover");
-});
-$(".custom1-select-trigger").on("click", function() {
-  $('html').one('click',function() {
-    $(".custom1-select").removeClass("opened");
+  $(".custom-option:first-of-type").hover(function() {
+    $(this).parents(".custom-options").addClass("option-hover");
+  }, function() {
+    $(this).parents(".custom-options").removeClass("option-hover");
   });
-  $(this).parents(".custom1-select").toggleClass("opened");
-  event.stopPropagation();
-});
-$(".custom1-option").on("click", function() {
-  $(this).parents(".custom1-select-wrapper").find("select").val($(this).data("value"));
-  $(this).parents(".custom1-options").find(".custom1-option").removeClass("selection");
-  $(this).addClass("selection");
-  $(this).parents(".custom1-select").removeClass("opened");
-  $(this).parents(".custom1-select").find(".custom1-select-trigger").text($(this).text());
-});
+  $(".custom-select-trigger").on("click", function() {
+    $('html').one('click', function() {
+      $(".custom-select").removeClass("opened");
+    });
+    $(this).parents(".custom-select").toggleClass("opened");
+    event.stopPropagation();
+  });
+  $(".custom-option").on("click", function() {
+    $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
+    $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+    $(this).addClass("selection");
+    $(this).parents(".custom-select").removeClass("opened");
+    $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+  });
+
+  /*日付プルダウン作成用スクリプト*/
+  $(".custom1-select").each(function() {
+    var classes = $(this).attr("class"),
+      id = $(this).attr("id"),
+      name = $(this).attr("name");
+    var template = '<div class="' + classes + '">';
+    template += '<span class="custom1-select-trigger">' + $(this).attr("placeholder") + '</span>';
+    template += '<div class="custom1-options">';
+    $(this).find("option").each(function() {
+      template += '<span class="custom1-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+    });
+    template += '</div></div>';
+
+    $(this).wrap('<div class="custom1-select-wrapper"></div>');
+    $(this).hide();
+    $(this).after(template);
+  });
+
+  $(".custom1-option:first-of-type").hover(function() {
+    $(this).parents(".custom1-options").addClass("option-hover");
+  }, function() {
+    $(this).parents(".custom1-options").removeClass("option-hover");
+  });
+  $(".custom1-select-trigger").on("click", function() {
+    $('html').one('click', function() {
+      $(".custom1-select").removeClass("opened");
+    });
+    $(this).parents(".custom1-select").toggleClass("opened");
+    event.stopPropagation();
+  });
+  $(".custom1-option").on("click", function() {
+    $(this).parents(".custom1-select-wrapper").find("select").val($(this).data("value"));
+    $(this).parents(".custom1-options").find(".custom1-option").removeClass("selection");
+    $(this).addClass("selection");
+    $(this).parents(".custom1-select").removeClass("opened");
+    $(this).parents(".custom1-select").find(".custom1-select-trigger").text($(this).text());
+  });
 </script>
-
-<div id="footer-fixed">
-    <img border="0" src="../common/kokoku.jpg" width="100%" height="100%">
-  </div>
 </body>
+
 </html>
