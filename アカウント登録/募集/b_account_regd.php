fa4ebea9804f974bbb97b222590e3655b22cf96c
Wada Takahiro <wada@WadanoMacBook-Pro.local>
2020-01-21 05:28:19 +0900

    インデント1

--- アカウント登録/募集/b_account_regd.php
+++ アカウント登録/募集/b_account_regd.php
@@ -63,92 +63,92 @@
 
 <!DOCTYPE html> <!-- 宣言（無くても機能する？） -->
 <html>
-<head>
-  <meta charset="utf-8"> <!-- 文字コードを宣言 -->
-  <title>PHP</title> <!-- ページのタイトル -->
-  <link rel="stylesheet" type="text/css" href="./CSS/common.css">
-  <link rel="stylesheet" type="text/css" href="./CSS/color.css">
-  <link rel="stylesheet" type="text/css" href="./CSS/size.css">
-  <link rel="stylesheet" type="text/css" href="./CSS/pop.css">
-</head>
-<body>
-  <div id="header-fixed">
-    <img border="0" src="header.jpg"style="vertical-align:middle;" width="100%" height="100%">
-    <a href= "regd_first.html">
-      <img border="0" src="back.jpg" width="20%" height="100%" class="back">
-    </a>
-  </div>
-
-
-  <div id="body-bk">
-    <div id="body" class="bg_test3">
-      <center> <!-- 中央寄せ -->
-        <h1> 募集側アカウント登録</h1>
-      </center>
+  <head>
+    <meta charset="utf-8"> <!-- 文字コードを宣言 -->
+    <title>PHP</title> <!-- ページのタイトル -->
+    <link rel="stylesheet" type="text/css" href="./CSS/common.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/color.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/size.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/pop.css">
+  </head>
+  <body>
+    <div id="header-fixed">
+      <img border="0" src="header.jpg"style="vertical-align:middle;" width="100%" height="100%">
+      <a href= "regd_first.html">
+        <img border="0" src="back.jpg" width="20%" height="100%" class="back">
+      </a>
     </div>
-    <div id="body" class="radio size1">
-      <form action ="#" method="get" name="myform" onsubmit="return check();">
-      <dl>
-      <center> <!-- 中央寄せ -->
-      <h2>
-        <dt>プロフィール画像</dt>
-        <dd><input type="file" name="pic"></dd>
-        <hr color="black"><br/>
-
-        <dt>会社・団体</dt>
-        <dd><input type = "text" name="groupname" id="a" value=""></dd>
-        <hr color="black"><br/>
-        <dt>メールアドレス</dt>
-        <dd><input type = "text" name="mail_address" id="b" value=""></dd>
-        <hr color="black"><br/><br/>
-        <dt>パスワード</dt>
-        <dd><input type = "text" name="password" id="c" value=""></dd>
-        <hr color="black"><br/><br/>
-        <dt>住所</dt>
-        <dd><input type = "text" name="user_address" id="d" value=""></dd>
-        <hr color="black"><br/><br/>
-        <dt>電話番号</dt>
-        <dd><input type = "text" name="tel_num" id="f" value=""></dd>
-        <hr color="black"><br/><br/>
-        <br>
-        <input type="submit" value="登録完了" class="btn-square1b"><br>
-      </center>
-    </form>
-  </div>
-  </div>
 
 
-  <script type="text/javascript">
-    function check() {
-      for(i = 0; i < document.myform.length; i++) {
-        if (document.myform.elements[i].type == "text") {
-          if (document.myform.elements[i].value.length <= 8) {
-              alert("登録に失敗しました");
-              return false;
-          }
-        }
-        if (i == 2){
+    <div id="body-bk">
+      <div id="body" class="bg_test3">
+        <center> <!-- 中央寄せ -->
+          <h1> 募集側アカウント登録</h1>
+        </center>
+      </div>
+      <div id="body" class="radio size1">
+        <form action ="#" method="get" name="myform" onsubmit="return check();">
+          <dl>
+          <center> <!-- 中央寄せ -->
+            <h2>
+            <dt>プロフィール画像</dt>
+            <dd><input type="file" name="pic"></dd>
+            <hr color="black"><br/>
+
+            <dt>会社・団体</dt>
+            <dd><input type = "text" name="groupname" id="a" value=""></dd>
+            <hr color="black"><br/>
+            <dt>メールアドレス</dt>
+            <dd><input type = "text" name="mail_address" id="b" value=""></dd>
+            <hr color="black"><br/><br/>
+            <dt>パスワード</dt>
+            <dd><input type = "text" name="password" id="c" value=""></dd>
+            <hr color="black"><br/><br/>
+            <dt>住所</dt>
+            <dd><input type = "text" name="user_address" id="d" value=""></dd>
+            <hr color="black"><br/><br/>
+            <dt>電話番号</dt>
+            <dd><input type = "text" name="tel_num" id="f" value=""></dd>
+            <hr color="black"><br/><br/>
+            <br>
+            <input type="submit" value="登録完了" class="btn-square1b"><br>
+          </center>
+        </form>
+      </div>
+    </div>
+
+
+    <script type="text/javascript">
+      function check() {
+        for(i = 0; i < document.myform.length; i++) {
           if (document.myform.elements[i].type == "text") {
-            if (document.myform.elements[i].value.length >= 8) {
-              alert("既に登録されたメールアドレスです");
-              return false;
+            if (document.myform.elements[i].value.length <= 8) {
+                alert("登録に失敗しました");
+                return false;
             }
           }
-        }
-        if (i == 3) {
-          if (document.myform.elements[i].type == "text") {
-            if (document.myform.elements[i].value.length <= 7) {
-              alert("登録に失敗しました");
-              return false;
+          if (i == 2){
+            if (document.myform.elements[i].type == "text") {
+              if (document.myform.elements[i].value.length >= 8) {
+                alert("既に登録されたメールアドレスです");
+                return false;
+              }
+            }
+          }
+          if (i == 3) {
+            if (document.myform.elements[i].type == "text") {
+              if (document.myform.elements[i].value.length <= 7) {
+                alert("登録に失敗しました");
+                return false;
+              }
             }
           }
         }
       }
-    }
-  </script>
+    </script>
 
-  <div id="footer-fixed">
-    <img border="0" src="kokoku.jpg" width="100%" height="100%">
-  </div>
-</body>
+    <div id="footer-fixed">
+      <img border="0" src="kokoku.jpg" width="100%" height="100%">
+    </div>
+  </body>
 </html>
