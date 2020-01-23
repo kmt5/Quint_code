72ea94ca0b40f999959d9e8bd90346b9805b7deb
Wada Takahiro <wada@WadanoMacBook-Pro.local>
2020-01-23 13:26:07 +0900

    login.phpのcss・画像パスの変更

--- VAISA/php/src/login.php
+++ VAISA/php/src/login.php
@@ -1,7 +1,7 @@
 <?php
   $mail = $_POST["mail_address"];
   $pswd = $_POST["password"];
-  //データベースに接続(test3)
+  //データベースに接続(vaisa)
   $dsn = "mysql:host=vaisa_mysql_1;dbname=vaisa;";
   $db = new PDO($dsn, 'root', 'root');
   $s_res = $db->query("select s_user_id,passwd from sanka_users where mail_address='".$mail."'");
@@ -37,15 +37,12 @@
   <head>
     <meta charset="utf-8"> <!-- 文字コードを宣言 -->
     <title>PHP</title> <!-- ページのタイトル -->
-    <link rel="stylesheet" type="text/css" href="./CSS/common.css">
-    <link rel="stylesheet" type="text/css" href="./CSS/color.css">
-    <link rel="stylesheet" type="text/css" href="./CSS/size.css">
-    <!--<link rel="stylesheet" type="text/css" href="./CSS/botan.css">-->
+    <link rel="stylesheet" type="text/css" href="./CSS/login.css">
   </head>
 
   <body>
     <div id="header-fixed">
-      <img border="0" src="./upload/header.jpg"style="vertical-align:middle;" width="100%" height="100%">
+      <img border="0" src="./common/header.jpg"style="vertical-align:middle;" width="100%" height="100%">
     </div>
 
     <div id="body-bk">
@@ -57,16 +54,15 @@
             <form action="#" name="login" method="post">
               <input type="text" name="mail_address" placeholder="メールアドレス"><br>
               <input type="password" name="password" placeholder="パスワード"><br>
-              <!--<a href= "javascript:login.submit()" class="btn-square3x">ログイン</a><br>-->
-              <input type="submit" value="ログイン" class="btn-square3x"><br>
+              <input type="submit" value="ログイン" class="btn-square3x"><br><!--ログインボタン-->
             </form>
-            <a href="manual.html" class="btn-square3y">アカウント登録</a><br>
+            <a href="manual.html" class="btn-square3y">アカウント登録</a><br><!--新規登録ボタン-->
           </center>
         </div>
       </div>
     </div>
     <div id="footer-fixed">
-      <img border="0" src="./upload/kokoku.jpg" width="100%" height="100%">
+      <img border="0" src="./common/kokoku.jpg" width="100%" height="100%">
     </div>
   </body>
 </html>
