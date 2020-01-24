ee328216538b0466c442fbebd062a9763fd55e30
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-01-24 17:08:11 +0900

    戻る実装,ホームに戻る実装、if文削除

--- VAISA/php/src/bosyu/2_sanka_list/b_entrant_vol_list.php
+++ VAISA/php/src/bosyu/2_sanka_list/b_entrant_vol_list.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $b_user_id = $_SESSION["b_user_id"];
-  
+
 $dsn = "mysql:host=vaisa_mysql_1;dbname=vaisa;";
 $db = new PDO($dsn, 'root', 'root');
 
@@ -18,17 +18,17 @@ $count = $db -> query("SELECT COUNT(vol_name) FROM volunteers WHERE b_user_id = 
 <head>
   <meta charset="utf-8"> <!-- 文字コードを宣言 -->
   <title>参加者一覧</title> <!-- ページのタイトル -->
-  <link rel="stylesheet" type="text/css" href="../common/common.css">
+  <link rel="stylesheet" type="text/css" href="../../common/common.css">
   <link rel="stylesheet" type="text/css" href="entrant.css">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
   <div id="header-fixed">
     <img border="0" src="../common/header.jpg" width="100%" height="100%">
-    <a href="javascript:history.back()">
+    <a href="b_entrant_login.php">
       <p id="back"><i class="fas fa-reply"></i></p>
     </a>
-    <a href="b_home">
+    <a href="../b_home.php">
       <p id="home"><i class="fas fa-home"></i></p>
     </a>
   </div>
@@ -42,24 +42,17 @@ $count = $db -> query("SELECT COUNT(vol_name) FROM volunteers WHERE b_user_id = 
         <?php
             $array_count = count($vol_name);
             for ($i = 0; $i < $array_count; $i++) {
-              if ($i != 2){
               echo "<form action='b_entrant_list.html' method='post'>";
               echo "<button type='submit' class='button-vol'>".$vol_name[$i]."</button>";
               echo "</form>";
               echo "<br>";
-            } else {
-              echo "<form action='b_entrant_list.html' method='post'>";
-              echo "<button type='submit' class='button-vol'><font color='red'><i class='fas fa-check'></i></font>　".$vol_name[$i]."</button>";
-              echo "</form>";
-              echo "<br>";
-            }
           }
           ?>
       </div>
     </div>
   </div>
   <div id="footer-fixed">
-    <img border="0" src="../common/kokoku.jpg" width="100%" height="100%">
+    <img border="0" src="../../common/kokoku.jpg" width="100%" height="100%">
   </div>
 </body>
 </html>
