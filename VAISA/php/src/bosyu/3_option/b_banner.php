3fc56e7d7862cdb8057f8502d9fc35f348cb3691
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-01-24 19:09:37 +0900

    戻る、ホームの遷移先を正しくした

--- VAISA/php/src/bosyu/3_option/b_banner.php
+++ VAISA/php/src/bosyu/3_option/b_banner.php
@@ -4,17 +4,20 @@
 <head>
   <meta charset="utf-8"> <!-- 文字コードを宣言 -->
   <title>検索上位表示</title> <!-- ページのタイトル -->
-  <link rel="stylesheet" type="text/css" href="../common/common.css">
+  <link rel="stylesheet" type="text/css" href="../../common/common.css">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="option.css">
 </head>
 
 <body>
   <div id="header-fixed">
-    <!-- ヘッダー箇所 -->
-    <img border="0" src="../common/header.jpg" width="100%" height="100%">
-    <img border="0" src="../common/back.jpg" width="20%" height="100%" class="back">
-    <img border="0" src="../common/home.jpg" width="20%" height="100%" class="home">
+    <img border="0" src="../../common/header.jpg" width="100%" height="100%">
+      <a href="b_option.php">
+        <p id="back"><i class="fas fa-reply"></i></p>
+      </a>
+      <a href="../b_home.php">
+        <p id="home"><i class="fas fa-home"></i></p>
+      </a>
   </div>
   <div id="body-bk">
     <div id="body">
@@ -97,7 +100,7 @@
       </script>
     </div>
     <div id="footer-fixed">
-      <img border="0" src="../common/kokoku.jpg" width="100%" height="100%">
+      <img border="0" src="../../common/kokoku.jpg" width="100%" height="100%">
     </div>
 </body>
 
