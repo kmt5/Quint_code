71de9696b961754d86d93ec5f69fa13b848dfc37
210360n <43773607+210360n@users.noreply.github.com>
2020-01-25 02:41:43 +0900

    Add files via upload

--- VAISA/php/src/sanka/4_qr/s_qr.php
+++ VAISA/php/src/sanka/4_qr/s_qr.php
@@ -8,27 +8,29 @@
   <meta charset="utf-8"> <!-- 文字コードを宣言 -->
   <title>PHP</title> <!-- ページのタイトル -->
   <link rel="stylesheet" type="text/css" href="./s_qr.css">
+  <link rel="stylesheet" type="text/css" href="../../common/common.css">
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
   <div id="header-fixed">
     <img border="0" src="../../common/header.jpg"style="vertical-align:middle;" width="100%" height="100%">
     <form method="post" name="back" action="../s_home.php">
       <input type="hidden" name="s_user_id" value="<?php echo $s_user_id; ?>" />
-      <a href="javascript:back.submit()">
-        <img border="0" src="../../common/back.jpg" width="20%" height="100%" class="back">
-      </a>
+      <button type="submit">
+        <p id="back"><i class="fas fa-reply"></i></p>
+        </button>
     </form>
     <form method="post" name="home" action="../s_home.php">
       <input type="hidden" name="s_user_id" value="<?php echo $s_user_id; ?>" />
-      <a href="javascript:home.submit()">
-        <img border="0" src="../../common/home.jpg" width="20%" height="100%" class="home">
-      </a>
+      <button type="submit">
+        <p id="home"><i class="fas fa-home"></i></p>
+        </button>
     </form>
   </div>
   <div id="body-bk">
-    <div id="body" class="bg_test">
+    <div id="Toptitle1">
       <center> <!-- 中央寄せ -->
-        <h1> QRコード</h1>
+        QRコード
       </center>
     </div>
     <div id="body">
