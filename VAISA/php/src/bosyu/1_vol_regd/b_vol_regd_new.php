7ff24eadb1432171758c723b6a071646c371e94d
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-01-24 17:58:08 +0900

    戻るとホームの遷移先を正しくした

--- VAISA/php/src/bosyu/1_vol_regd/b_vol_regd_new.php
+++ VAISA/php/src/bosyu/1_vol_regd/b_vol_regd_new.php
@@ -23,7 +23,7 @@ if ($pref_data = $db->query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
 <head>
   <meta charset="utf-8"> <!-- 文字コードを宣言 -->
   <title>Sample</title> <!-- ページのタイトル -->
-  <link rel="stylesheet" type="text/css" href="../../CSS/common.css">
+  <link rel="stylesheet" type="text/css" href="../../common/common.css">
   <link rel="stylesheet" type="text/css" href="./CSS/vol_regd.css">
   <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
@@ -33,8 +33,12 @@ if ($pref_data = $db->query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
 <body>
   <div id="header-fixed">
     <img border="0" src="../../common/header.jpg" width="100%" height="100%">
-    <img border="0" src="../../common/back.jpg" width="20%" height="100%" class="back">
-    <img border="0" src="../../common/home.jpg" width="20%" height="100%" class="home">
+    <a href="b_vol_regd.php">
+      <p id="back"><i class="fas fa-reply"></i></p>
+    </a>
+    <a href="../b_home.php">
+      <p id="home"><i class="fas fa-home"></i></p>
+    </a>
   </div>
   <div id="body-bk">
     <div id="body">
@@ -44,15 +48,24 @@ if ($pref_data = $db->query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
       <div width="100%" class="new">
         <h1 align="center">ボランティア内容</h1>
         <form method="POST" action="update_new.php" enctype="multipart/form-data">
-          <h2>ボランティアイメージ画像</h2>
-          <input type="file" name="image" accept="image/*">
-
           <h2>ボランティア名</h2>
-          <input type="text" name="vol_name" maxlength="20" value="" placeholder="２０文字以内で入力">
+          <input type="text" name="vol_name" maxlength="20" value="" placeholder="２０文字以内で入力" required>
           <br>
+          <h2>イメージ画像</h2>
+          <input type="file" id='image' name="image" accept="image/*">
+          <img id="preview">
+          <script>
+            $('#image').on('change', function (e) {
+              var reader = new FileReader();
+              reader.onload = function (e) {
+                $("#preview").attr('src', e.target.result);
+              }
+              reader.readAsDataURL(e.target.files[0]);
+            });
+          </script>
           <h2>地域選択</h2>
           <label>都道府県</label>
-          <select name="select_pref" , id="pref">
+          <select name="select_pref" , id="pref" required>
             <option value="none" selected>----選択してください----</option>
             <?php
             echo $pref_pd;
@@ -69,25 +82,25 @@ if ($pref_data = $db->query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
             });
           </script>
           <label>地域</label>
-          <select name="select_area" , id="area">
+          <select name="select_area" , id="area" required>
             <option value="none" selected></option>
 
           </select>
           <br>
           <h2>郵便番号</h2>
           <!-- ▼郵便番号入力フィールド(7桁) -->
-          <input type="text" name="zip11" size="10" maxlength="8" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11');">
+          <input type="text" name="zip11" size="10" maxlength="8" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11');" required>
           <!-- ▼住所入力フィールド(都道府県+以降の住所) -->
           <br>
           <h2>住所</h2>
-          <textarea name="addr11" placeholder="住所を入力（郵便番号を入力すると自動で分かる箇所まで表示します）"></textarea>
+          <textarea name="addr11" placeholder="住所を入力（郵便番号を入力すると自動で分かる箇所まで表示します）" required></textarea>
           <!--<input type="text" name="addr11" size="10" width="80%"> -->
           <br>
           <h2>開催日</h2>
-          <select id="year" name="year">
+          <select id="year" name="year" required>
             <option value="2020">2020</option>
           </select> 年
-          <select id="month" name="month">
+          <select id="month" name="month" required>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -101,7 +114,7 @@ if ($pref_data = $db->query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
             <option value="11">11</option>
             <option value="12">12</option>
           </select> 月
-          <select id="day" name="day">
+          <select id="day" name="day" required>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -136,16 +149,16 @@ if ($pref_data = $db->query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
           </select> 日
           <br>
           <label>開始時間</label>
-          <input type="time" name="beg_time">
+          <input type="time" name="beg_time" required>
           <br>
           <label>終了時間</label>
-          <input type="time" name="fin_time">
+          <input type="time" name="fin_time" required>
           <h2>定員</h2>
-          <input class="capacity" type="number" name="capacity" value="" maxlength="3">名
+          <input class="capacity" type="number" name="capacity" value="" maxlength="3" required>名
           <br><br>
-          <input id="check-a" type="checkbox" name="va_flag" value="1" checked><label for="check-a">報酬あり</label><br>
+          <input id="check-a" type="checkbox" name="va_flag" value="1"><label for="check-a">報酬あり</label><br>
           (※詳しい報酬内容は<br>　　詳細にお書きください)<br><br>
-          <input id="check-b" type="checkbox" name="newbie_flag" value="1" checked><label for="check-a">初心者歓迎</label><br><br>
+          <input id="check-b" type="checkbox" name="newbie_flag" value="1"><label for="check-a">初心者歓迎</label><br><br>
           <?php
           $specData = $db->query("SELECT DISTINCT rank_spec_flag FROM options WHERE b_user_id = $b_user_id");
           foreach ($specData as $data_val) {
@@ -166,7 +179,7 @@ if ($pref_data = $db->query("SELECT DISTINCT pref_id, pref_name FROM areas")) {
           ?>
 
           <h2>詳細</h2>
-          <textarea name="detail" placeholder="詳細を入力"></textarea>
+          <textarea name="detail" placeholder="詳細を入力" required></textarea>
           <br>
           <input type='hidden' name='b_user_id' value="<?php echo $b_user_id; ?>">
           <button type="submit" align="center">登録</button>
