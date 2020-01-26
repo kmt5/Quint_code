51b91b834c63afea4cd837e9634edc1ed25b3fa6
Wada Takahiro <wada@WadanoMacBook-Pro.local>
2020-01-26 20:24:06 +0900

    tmp

--- VAISA/php/src/sanka/s_account_regd.php
+++ VAISA/php/src/sanka/s_account_regd.php
@@ -62,7 +62,7 @@
           $b_cnt = $b_cnt->fetchColumn();
         }
       }
-       while ($s_cnt != 0 or $b_cnt != 0);
+      while ($s_cnt != 0 or $b_cnt != 0);
 
       /*$msg = null;
       // もし$_FILES['pic']があって、一時的なファイル名の$_FILES['pic']が
