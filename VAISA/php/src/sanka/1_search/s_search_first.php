f8ca83626a0381e81b540056112cff51857e9efa
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-01-26 16:48:14 +0900

    formに書き換えた

--- VAISA/php/src/sanka/1_search/s_search_first.php
+++ VAISA/php/src/sanka/1_search/s_search_first.php
@@ -42,7 +42,7 @@ $s_user_id = $_POST["s_user_id"];
   </div>
 
   <div id="footer-fixed">
-    <img border="0" src="../common/kokoku.jpg" width="100%" height="100%">
+    <img border="0" src="../../common/kokoku.jpg" width="100%" height="100%">
   </div>
 </body>
 </html>
