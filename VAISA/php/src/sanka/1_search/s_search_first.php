917ac54b9386183c3b8005191676715e956a7d56
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-01-26 16:34:40 +0900

    formに書き換えた

--- VAISA/php/src/sanka/1_search/s_search_first.php
+++ VAISA/php/src/sanka/1_search/s_search_first.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $s_user_id = $_POST["s_user_id"];
 ?>
 <!DOCTYPE html> <!-- 宣言（無くても機能する？） -->
@@ -6,19 +6,23 @@ $s_user_id = $_POST["s_user_id"];
 <head>
   <meta charset="utf-8"> <!-- 文字コードを宣言 -->
   <title>検索</title> <!-- ページのタイトル -->
-  <link rel="stylesheet" type="text/css" href="./CSS/common.css">
+  <link rel="stylesheet" type="text/css" href="../../common/common.css">
   <link rel="stylesheet" type="text/css" href="./CSS/search_first.css">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
   <div id="header-fixed">
-    <img border="0" src="../common/header.jpg" width="100%" height="100%">
-    <a href="javascript:history.back()">
+    <img border="0" src="../../common/header.jpg" width="100%" height="100%">
+    <form method="post" name="form1" action="../s_home.php">
+    <a href="javascript:form1.submit()">
       <p id="back"><i class="fas fa-reply"></i></p>
     </a>
-    <a href="s_home">
+  </form>
+  <form method="post" name="form1" action="../s_home.php">
+    <a href="javascript:form1.submit()">
       <p id="home"><i class="fas fa-home"></i></p>
     </a>
+  </form>
   </div>
 
   <div id = "body-bk">
@@ -26,8 +30,12 @@ $s_user_id = $_POST["s_user_id"];
       <div id ="Toptitle1">
         検索
       </div>
-        <a href="s_search_area_spe.php" class="btn-point1">地域を選択して検索</a>
-        <a href="s_search_area_edit.php" class="btn-point2">登録地域周辺で検索</a>
+      <form method="post" name="form1" action="s_search_area_spe.php">
+        <a href="javascript:form1.submit()" class="btn-point1">地域を選択して検索</a>
+      </form>
+      <form method="post" name="form1" action="s_search_area_edit.php">
+        <a href="javascript:form1.submit()" class="btn-point2">登録地域周辺で検索</a>
+      </form>
     </div>
   </div>
 
