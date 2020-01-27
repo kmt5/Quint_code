944f26d2277d49b850f4abf36cd0841a7403dba1
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-01-27 10:23:55 +0900

    idを受け渡す処理を追加

--- VAISA/php/src/bosyu/3_option/b_banner.php
+++ VAISA/php/src/bosyu/3_option/b_banner.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+$b_user_id = $_POST["b_user_id"];
+echo $b_user_id;
+$_SESSION["b_user_id"] = $b_user_id;
+?>
+
 <!DOCTYPE html> <!-- 宣言（無くても機能する？） -->
 <html>
 
@@ -12,12 +19,18 @@
 <body>
   <div id="header-fixed">
     <img border="0" src="../../common/header.jpg" width="100%" height="100%">
-      <a href="b_option.php">
+      <form method="post" name="formback" action="b_option.php">
+        <input type="hidden" name="b_user_id" value="<?php echo $b_user_id; ?>" />
+      <a href="javascript:formback.submit()">
         <p id="back"><i class="fas fa-reply"></i></p>
       </a>
-      <a href="../b_home.php">
+    </form>
+    <form method="post" name="formhome" action="../b_home.php">
+      <input type="hidden" name="b_user_id" value="<?php echo $b_user_id; ?>" />
+      <a href="javascript:formhome.submit()">
         <p id="home"><i class="fas fa-home"></i></p>
       </a>
+    </form>
   </div>
   <div id="body-bk">
     <div id="body">
