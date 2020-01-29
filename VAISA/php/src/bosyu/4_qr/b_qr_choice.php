34fae27baa8f2fec5d8af3267eb0ed43ad26309b
210360n <43773607+210360n@users.noreply.github.com>
2020-01-29 21:11:25 +0900

    Add files via upload

--- VAISA/php/src/bosyu/4_qr/b_qr_choice.php
+++ VAISA/php/src/bosyu/4_qr/b_qr_choice.php
@@ -18,15 +18,15 @@
   <body>
   <div id="header-fixed">
     <img border="0" src="../../common/header.jpg" width="100%" height="100%">
-    <form method="post" name="back" action="../b_home.php">
+    <form method="post" name="formback" action="../b_home.php">
       <input type="hidden" name="b_user_id" value="<?php echo $b_user_id; ?>" />
-      <a href="javascript:back.submit()">
+      <a href="javascript:formback.submit()">
         <p id="back"><i class="fas fa-reply"></i></p>
       </a>
     </form>
-    <form method="post" name="home" action="../b_home.php">
+    <form method="post" name="formhome" action="../b_home.php">
       <input type="hidden" name="b_user_id" value="<?php echo $b_user_id; ?>" />
-      <a href="javascript:home.submit()">
+      <a href="javascript:formhome.submit()">
         <p id="home"><i class="fas fa-home"></i></p>
       </a>
     </form>
