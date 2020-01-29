15a5c36f08bfdf5e880946d533ff126c7917ed26
210360n <43773607+210360n@users.noreply.github.com>
2020-01-29 15:56:09 +0900

    Add files via upload

--- VAISA/php/src/sanka/s_home.php
+++ VAISA/php/src/sanka/s_home.php
@@ -67,7 +67,7 @@
 
 
     <div id="footer-fixed">
-      <img border="0" src="../common/kokoku.jpg" width="100%" height="70%">
+      <img border="0" src="../common/kokoku.jpg" width="100%" height="100%">
     </div>
   </body>
 </html>
