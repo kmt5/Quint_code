a74430622891fdecc3f17357e02b34a0f2d3ef5d
Myojin Yuko <yukirena01@gmail.com>
2020-01-30 00:49:13 +0900

    upd

--- VAISA/php/src/sanka/2_myvol/s_my_first.php
+++ VAISA/php/src/sanka/2_myvol/s_my_first.php
@@ -20,7 +20,7 @@ $user_id = $_POST['s_user_id'];
       </a>
     </form>
     <form method="post" name="formhome" action="../s_home.php">
-      <input type="hidden" name="s_user_id" value="<?php echo $b_user_id; ?>" />
+      <input type="hidden" name="s_user_id" value="<?php echo $s_user_id; ?>" />
       <a href="javascript:formhome.submit()">
         <p id="home"><i class="fas fa-home"></i></p>
       </a>
