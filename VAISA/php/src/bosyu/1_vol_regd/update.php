9151012d5566135ecc2c262d52c5b5f8188fb431
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-01-30 09:01:09 +0900

    デザインの追加

--- VAISA/php/src/bosyu/1_vol_regd/update.php
+++ VAISA/php/src/bosyu/1_vol_regd/update.php
@@ -126,31 +126,23 @@ $db = null;
 
         <h1 align="center">以下の内容で<br>再登録しました</h1>
 
-        <h2>ボランティア名</h2>
+        <h2 class="a">ボランティア名</h2>
+        <div class="textarea">
         <?php echo $vol_name; ?>
-        <br>
-        <h2>ボランティアイメージ画像</h2>
-        <?php
-        if ($vol_fig_path == null) {
+        </div>
+        <h2 class="a">ボランティアイメージ画像</h2>
+        <?php if ($vol_fig_path == null) {
           echo "<br>登録されている写真はありません。";
         } else {
-          echo "<img src=../" . $vol_fig_path . ">";
-        }
-        ?>
-        <h2>地域選択</h2>
-        <label>都道府県　</label>
-        <?php echo $pref_name; ?>
-        <br><br>
-        <label>地域　　　</label>
-        <?php echo $area_name; ?>
-        <br>
-        <h2>郵便番号</h2>
-        <?php echo $post_num; ?>
-        <br>
-        <h2>住所</h2>
+          echo "<img id='preview' src=../" . $vol_fig_path . ">";
+        } ?>
+        <h2 class="a">都道府県　：<?php echo $pref_name; ?></h2>
+        <h2 class="a">地域　　　：<?php echo $area_name; ?></h2>
+        <h2 class="a">〒　<?php echo $post_num; ?></h2>
+        <div class="textarea">
         <?php echo $vol_place; ?>
-        <br>
-        <h2>開催日</h2>
+        </div>
+        <h2 class="a">開催日</h2>
         <?php echo $vol_date; ?>
         <br>
         <label>開始時間</label>
@@ -158,27 +150,24 @@ $db = null;
         <br>
         <label>終了時間</label>
         <?php echo $vol_fin_time; ?>
-        <h2>定員</h2>
-        <?php echo $vol_capacity; ?>
+        <h2 class="a">定員　：<?php echo $vol_capacity; ?>名</h2>
+
         <?php if ($val_flag == 1) {
           echo "<p class='dezain'>報酬あり</p>";
         } else {
           echo "<p class='dezain'>報酬なし</p>";
         } ?>
+
         <?php if ($newbie_flag == 1) {
           echo "<p class='dezain'>初心者歓迎</p>";
         } else {
           echo "<p class='dezain'>経験者のみ</p>";
         } ?>
-        <label>ランク指定　</label>
-        <?php if ($spec_rank == 0) {
-          echo "<p class='dezain'>指定なし</p>";
-        } else {
-          echo "<p class='dezain'>$spec_rank</p>";
-        } ?>
-        <h2>詳細</h2>
+
+        <h2 class="a">ランク指定　：<?php echo $spec_rank; ?></h2>
+        <h2 class="a">詳細</h2>
         <div class="textarea">
-          <?php echo $vol_detail; ?>
+        <?php echo $vol_detail; ?>
         </div>
         <br>
         <br>
